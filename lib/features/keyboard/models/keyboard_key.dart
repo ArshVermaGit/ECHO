a8a9de9e210b147612d92555a1f870dbd92e920d
Arsh Verma <arshverma.dev@gmail.com>
2026-02-25 00:13:48 +0530

    Update keyboard_key.dart

---
+++ lib/features/keyboard/models/keyboard_key.dart
@@ -0,0 +1,17 @@
+class KeyboardKey {
+  final String label;
+  final String? value;
+  final double x;
+  final double y;
+  final double width;
+  final double height;
+
+  KeyboardKey({
+    required this.label,
+    this.value,
+    required this.x,
+    required this.y,
+    required this.width,
+    required this.height,
+  });
+}
