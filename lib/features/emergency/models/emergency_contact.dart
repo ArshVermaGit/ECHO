e197b345d3306f6f790c12a228d3a5a339f41da6
Arsh Verma <arshverma.dev@gmail.com>
2026-02-25 00:13:31 +0530

    Update emergency_contact.dart

---
+++ lib/features/emergency/models/emergency_contact.dart
@@ -0,0 +1,15 @@
+class EmergencyContact {
+  final String id;
+  final String name;
+  final String phoneNumber;
+  final String relationship;
+  final int sortOrder;
+
+  EmergencyContact({
+    required this.id,
+    required this.name,
+    required this.phoneNumber,
+    required this.relationship,
+    required this.sortOrder,
+  });
+}
